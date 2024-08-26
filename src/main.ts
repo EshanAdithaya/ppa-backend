cd6ee39b50a8e9b107a3bccbe9f132a413f62a6d
S. D. E. A. Gunathilaka <87906517+EshanAdithaya@users.noreply.github.com>
2024-08-26 13:52:39 +0530

    chore: Enable CORS for API endpoints

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+ 
   const config = new DocumentBuilder()
     .setTitle('Fitness Club API')
     .setDescription('The Fitness Club API description')
@@ -15,7 +15,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
